20bb36949d06f8eb19dcefc4c84294d3971e96bb
kurageya0307 <30034483+kurageya0307@users.noreply.github.com>
2022-07-08 17:16:03 +0900

    Refactoring to use all segment space, now fullspec drawing is calculated in 74 sec.!

--- logic/remove_overhangs_on_both_edges.py
+++ logic/remove_overhangs_on_both_edges.py
@@ -18,233 +18,75 @@ from numba import jit
 
 RATIO = 0.5
 
-class Segment2:
-    def __init__(self, p0, p1):
-        self.__x0 = p0.x
-        self.__y0 = p0.y
-        self.__x1 = p1.x
-        self.__y1 = p1.y
+from pyqtree import Index
 
-        self.__min_x = self.__x0 if self.__x0 < self.__x1 else self.__x1
-        self.__max_x = self.__x0 if self.__x0 > self.__x1 else self.__x1
-        self.__min_y = self.__y0 if self.__y0 < self.__y1 else self.__y1
-        self.__max_y = self.__y0 if self.__y0 > self.__y1 else self.__y1
+def getEdgeRemovedCurve(curve, segment_space):
+    removed_curve = LinearApproximateCurve()
 
-        self.__center_x = (self.__min_x + self.__max_x)/2.0
-        self.__center_y = (self.__min_y + self.__max_y)/2.0
-
-        self.__width  = self.__max_x - self.__min_x
-        self.__height = self.__max_y - self.__min_y
-    #end
-
-    def intersection(self, other_segment):
-        if self.testCollision(other_segment):
-            det = (self.__max_x - self.__min_x) * (other_segment.max_y - other_segment.min_y) - (other_segment.max_x - other_segment.min_x) * (self.__max_y - self.__min_y)
-            t = ((other_segment.max_y - other_segment.min_y) * (other_segment.max_x - self.__min_x) + (other_segment.min_x - other_segment.max_x) * (other_segment.max_y - self.__min_y)) / det
-            x = t * self.__max_x + (1.0 - t) * self.__min_x
-            y = t * self.__max_y + (1.0 - t) * self.__min_y
-            return [Point(x, y, evaluate=False)]
-        #end if
-        return []
-    #end
-
-    def testCollision(self, other_segment):
-        delta_x = abs( other_segment.center_x - self.__center_x )
-        delta_y = abs( other_segment.center_y - self.__center_y )
-
-        sum_half_width  = ( other_segment.width  + self.__width  ) / 2.0
-        sum_half_height = ( other_segment.height + self.__height ) / 2.0
-
-        if ( (delta_x < sum_half_width) & (delta_y < sum_half_height) ):
-            return True
-        else:
-            return False
-        #end if
-    #end
-
-    @property
-    def max_x(self):
-        return self.__max_x
-    #end
-    @property
-    def max_y(self):
-        return self.__max_y
-    #end
-    @property
-    def min_x(self):
-        return self.__min_x
-    #end
-    @property
-    def min_y(self):
-        return self.__min_y
-    #end
-    @property
-    def center_x(self):
-        return self.__center_x
-    #end
-    @property
-    def center_y(self):
-        return self.__center_y
-    #end
-    @property
-    def width(self):
-        return self.__width
-    #end
-    @property
-    def height(self):
-        return self.__height
-    #end
-#end
-
-def getRemoveStartingIndexAndPoint(tip_or_terminal, other_points_sets):
-    for target_point_index in range( len(tip_or_terminal)-1 ):
-        target_segment = Segment( tip_or_terminal[target_point_index], tip_or_terminal[target_point_index+1], evaluate=False )
-        #target_segment = Segment2( tip_or_terminal[target_point_index], tip_or_terminal[target_point_index+1])
-        for other_points in other_points_sets:
-            for other_point_index in range( len(other_points)-1 ):
-                if tip_or_terminal[target_point_index] == other_points[other_point_index]:
-                    continue
-                if tip_or_terminal[target_point_index+1] == other_points[other_point_index]:
-                    continue
-                if tip_or_terminal[target_point_index] == other_points[other_point_index+1]:
-                    continue
-                if tip_or_terminal[target_point_index+1] == other_points[other_point_index+1]:
-                    continue
+    intersect_points = []
+    intersect_indexes = []
+    for i in range( len(curve)-1 ):
+        the_segment = curve.getSegment(i)
+        the_segment_rect = curve.getSegmentRectTuple(i)
+        other_segments = segment_space.intersect(the_segment_rect)
+        for other_segment in other_segments:
+            if the_segment == other_segment:
+                continue
+            if (the_segment.p1==other_segment.p2):
+                continue
+            if (the_segment.p2==other_segment.p1):
+                continue
                 
-                other_segment = Segment( other_points[other_point_index], other_points[other_point_index+1], evaluate=False)
-                #other_segment = Segment2( other_points[other_point_index], other_points[other_point_index+1])
-                intersect = target_segment.intersection(other_segment)
-                if intersect != [] and type(intersect[0]) != Segment2D:
-                    ##print(intersect[0])
-                    ##print("------------------------------------------------------------------------------------------------------")
-                    ##for p in tip_or_terminal:
-                    ##    print(p)
-                    # and intersect[0] != start_point and intersect[0] != end_point:
-                    return target_point_index, intersect[0]
-                    #end if
-                #end if
-            #end
+            inter = the_segment.intersection(other_segment)
+            if inter != [] and type(inter[0]) != Segment2D:
+                intersect_points.append(inter[0])
+                intersect_indexes.append(i)
+            #end if
         #end
     #end
-    return None, None
-#end
 
-def getPointIntersectionOfCurve(linear_approximate_curve, one_curve, space_index):
-    split_points, split_rects = one_curve.getSplittedPointsAndRectangulars()
+    if len(intersect_points) == 0:
+        return curve
+    elif len(intersect_points) == 1:
+        dist_start_intersect = curve[0].distance(intersect_points[0])
+        dist_end_intersect   = curve[-1].distance(intersect_points[0])
+        dist_start_end       = curve[0].distance(curve[-1])
 
-    split_index = 0
-    start_split_index = 0
-    start_local_index = 0
-    start_point = None
+        #if (dist_start_end*0.3 > dist_start_intersect) and (dist_start_end*0.3 > dist_end_intersect):
+        #    return curve
+        ##endif
+        #not work ummm...
 
-    max_check_split_num = math.ceil( len(split_points)*0.3 )
-    #print(max_check_split_num)
-    #print(len(split_points))
-    for points, rect in zip(split_points, split_rects):
-        bbox_tupple = (rect.q.x, rect.q.y, rect.m.x, rect.m.y)
-        other_points_sets = space_index.intersect(bbox_tupple)
-
-        check_len = len(points)
-        for ps in other_points_sets:
-            check_len += len(ps)
-        #end for
-
-        #print("check_len, {}".format(check_len))
-        index, point = getRemoveStartingIndexAndPoint(points, other_points_sets)
-        #print("final removed tip : ", point )
-
-        if index is not None:
-            start_split_index = split_index
-            start_local_index = index
-            start_point = point
-            break
-        #end if
-        if split_index >= max_check_split_num:
-            break
-        #end if
-        split_index += 1
-    #end for
-    #print(start_local_index)
-
-    reverse_split_points = list( reversed(split_points) )
-    reverse_split_rects = list( reversed(split_rects) )
-
-    split_index = 0
-    end_split_index = len(split_points)
-    end_local_index = None
-    end_point = None
-    for points, rect in zip(reverse_split_points, reverse_split_rects):
-        bbox_tupple = (rect.q.x, rect.q.y, rect.m.x, rect.m.y)
-        other_points_sets = space_index.intersect(bbox_tupple)
-
-        #print("check_len, {}".format(check_len))
-        reverse_points = list( reversed(points) )
-        index, point = getRemoveStartingIndexAndPoint(reverse_points, other_points_sets)
-        #print("final removed terminal : ", point )
-
-        if index is not None:
-            end_split_index = len(split_points) - split_index
-            end_local_index = len(points) - index
-            end_point = point
-            break
-        #end if
-
-        if True:#split_index >= max_check_split_num:
-            break
-        #end if
-        split_index += 1
-    #end for
-
-    if start_point == end_point:
-        return one_curve.points
-    #end if
-    #print(end_split_index)
-    #print(end_local_index)
-    #print(end_point)
-
-    removed_points = []
-    for i in range(start_split_index, end_split_index):
-        points = split_points[i]
-        if i == start_split_index:
-            if start_point is not None:
-                removed_points.append( start_point )
-            #end if
-            #print(start_local_index)
-            for j in range(start_local_index, len(points)):
-                removed_points.append( points[j] )
+        if dist_start_intersect < dist_end_intersect:
+            for i in range( intersect_indexes[0], len(curve) ):
+                removed_curve.append( curve[i] )
             #end for
-        elif i == end_split_index-1:
-            end_local_index = len(points) if end_local_index is None else end_local_index
-            #print(end_local_index)
-
-            for j in range(0, end_local_index):
-                removed_points.append( points[j] )
-            #end for
-            if end_point is not None:
-                removed_points.append( end_point )
-            #end if
+            return removed_curve
         else:
-            for j in range(0, len(points)):
-                removed_points.append( points[j] )
+            for i in range(intersect_indexes[0]+1):
+                removed_curve.append( curve[i] )
             #end for
+            return removed_curve
         #end if
-    #end for
-    return removed_points
+    else:
+        for i in range( intersect_indexes[0], intersect_indexes[-1]+1 ):
+            removed_curve.append( curve[i] )
+        #end for
+        return removed_curve
+    #end if
 #end
 
-def removeOverHangs(linear_approximate_curve, space_index):
+
+def removeOverHangs(linear_approximate_curve, segment_space):
     all_layer_removed_curves = AllLayerCurveSet()
     total_layer_num = len(linear_approximate_curve)
     layer_index = 0
     for layer_name_one, curve_set_in_one_layer in linear_approximate_curve:
         removed_curve_set = CurveSetInALayer()
         total_curve_num_in_a_layer = len(curve_set_in_one_layer)
-        for j, one_curve in enumerate(curve_set_in_one_layer):
-            removed_curve = LinearApproximateCurve()
+        for j, curve in enumerate(curve_set_in_one_layer):
             print("remove edge {}/{} in {} {}/{}".format(j+1, total_curve_num_in_a_layer, layer_name_one, layer_index+1, total_layer_num))
-            for point in getPointIntersectionOfCurve(linear_approximate_curve, one_curve, space_index):
-                removed_curve.append( point )
-            #end for
+            removed_curve = getEdgeRemovedCurve(curve, segment_space)
             removed_curve_set.append(removed_curve)
         #end for
         all_layer_removed_curves.append(layer_name_one, removed_curve_set)
--- test/logic/test_remove_overhangs_on_both_edges.py
+++ test/logic/test_remove_overhangs_on_both_edges.py
@@ -3,6 +3,7 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../logic'))
 from remove_overhangs_on_both_edges import *
+from make_segment_space import makeSegmentSpace
 from convert_bezier_to_linear_approximate_curve import convertBezierToLinearApproximateCurve
 from read_cubic_bezier_curve_from_svg_file import readCubicBezierCurveFromSvgFile
 
@@ -13,22 +14,25 @@ import unittest
 
 class TestApproximateCurveWithLineSegments(unittest.TestCase):
     def testConvertBezierToLinearApproximateCurve(self):
-        #cubic_bezier_curve, space_index = readCubicBezierCurveFromSvgFile("data/RemoveEdgeTest.svg")
         diff = ""
         diff += '<g id="Hair" vectornator:layerName="Hair">\n'
         diff += '<path d="M548.062 246.894C548.218 246.794 549.608 250.739 549.667 250.919C550.314 252.87 550.674 254.906 551.093 256.919C552.808 265.178 553.842 274.954 552.041 283.289" fill="none" fill-rule="evenodd" opacity="1" stroke="#1f0a15" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>\n'
         diff += '<path d="M559.333 244.804C560.218 249.357 558.701 255.18 557.924 259.668C556.196 269.647 553.156 279.346 548.897 288.514" fill="none" fill-rule="evenodd" opacity="1" stroke="#1f0a15" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>\n'
         diff += '</g>\n'
 
-        #cubic_bezier_curve, space_index = readCubicBezierCurveFromSvgFile("data/NgData.svg")
-        cubic_bezier_curve, space_index = readCubicBezierCurveFromSvgFile("data/KarinFace.svg")
-        #cubic_bezier_curve, space_index = readCubicBezierCurveFromSvgFile("data/HalfFace.svg")
-        linear_approximate_curve = convertBezierToLinearApproximateCurve(cubic_bezier_curve, space_index, 1.0, 10)
+        #cubic_bezier_curve, view_box_data = readCubicBezierCurveFromSvgFile("data/RemoveEdgeTest.svg")
+        #cubic_bezier_curve, view_box_data = readCubicBezierCurveFromSvgFile("data/NgData.svg")
+        #cubic_bezier_curve, view_box_data = readCubicBezierCurveFromSvgFile("data/KarinFace.svg")
+        cubic_bezier_curve, view_box_data = readCubicBezierCurveFromSvgFile("data/TaimaninKarin.svg")
+        #cubic_bezier_curve, view_box_data = readCubicBezierCurveFromSvgFile("data/HalfFace.svg")
+        linear_approximate_curve = convertBezierToLinearApproximateCurve(cubic_bezier_curve, 1.0, 10)
         #writeSvg("fuga.svg", linear_approximate_curve, color="#ff0000", diff_str=diff)
+
+        segment_space = makeSegmentSpace(linear_approximate_curve, view_box_data)
 
         diff += linear_approximate_curve.to_svg_str(color="#ff0000")
 
-        all_layer_removed_curves = removeOverHangs(linear_approximate_curve, space_index)
+        all_layer_removed_curves = removeOverHangs(linear_approximate_curve, segment_space)
         writeSvg("hoge.svg", all_layer_removed_curves, color="#008800", shift=0.0, diff_str=diff)
 #        print( all_layer_removed_curves.to_svg_str() )
     #end
